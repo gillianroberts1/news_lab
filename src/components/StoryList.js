b07731ab16fc88a71772debae555bfbf6cbd5f44
gillian <gillianroberts@me.com>
2023-09-19 15:42:17 +0100

    hooking stories

---
+++ src/components/StoryList.js
@@ -0,0 +1,19 @@
+const StoryList = ({news}) => {
+
+  
+const storyTitles = news.map((story) => {
+    return ( story.title)})
+
+
+    return (
+<p>
+    {storyTitles}
+</p>
+    )
+
+    }
+
+
+    export default StoryList
+    
+
--- src/containers/NewsContainer.js
+++ src/containers/NewsContainer.js
@@ -1,50 +1,38 @@
-const { useState, useEffect } = require("react")
+import React, { useState, useEffect } from "react";
+import StoryList from "../components/StoryList";
 
 const NewsContainer = () => {
+  const [news, setNews] = useState([]);
 
-    const [news, setNews] = useState([])
-
-    
-
-    useEffect(() => {
-        fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
-        .then( res => res.json())
-        .then (data => setNews(data))
-    }, [])
-}
-
-    const newsList = news.map((story) => {
-        return (
-            fetch(story).then(res => res.json)
-            .then(data => {data.map((storyId) => {
-            return fetch(`https://hacker-news.firebaseio.com/v0/item/{storyId}.json`)
-            .then (res => res.json())
-           
-            
-            })
-
-    
-
-    
-
-    // Promise.all(storyPromises)
-    
+  useEffect(() => {
+    fetch("https://hacker-news.firebaseio.com/v0/topstories.json")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        const storyPromises = data.map((storyId) => {
+            console.log(storyId)
+            return fetch(
+              `https://hacker-news.firebaseio.com/v0/item/${storyId}.json`
+            ).then((res) => res.json());
+          });
+       
+          
+  Promise.all(storyPromises)
+  .then(results => setNews(results))
+  
+      
+  
+        })
+    },[])
 
 
-        
-    
+  return (
+  
+  <>
+  <StoryList news={news}/>
+  </>
+  )
 
-// const newsList = news.map((story) => {
-//     return fetch()
+};
 
-// })
-return (
-    
-        <>
-        {newsList}
-        </>
-    )
-}
-
-
-export default NewsContainer
+export default NewsContainer;
